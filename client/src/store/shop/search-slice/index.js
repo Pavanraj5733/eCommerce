d4c2a3a42210df882400ba2ee35110241dd62ff6
Pavan Raj <70580158+Pavanraj5733@users.noreply.github.com>
2025-07-01 14:19:34 +0530

    Update index.js

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `https://ecommerce-b5ff.onrender.com/api/shop/search/${keyword}`
+      `https://ecommerce-b5ff.onrender.com/shop/search/${keyword}`
     );
 
     return response.data;
