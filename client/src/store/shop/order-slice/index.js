3649c0d2a0dc5ce9aa8f075e926f31c0ce232299
Pavan Raj <70580158+Pavanraj5733@users.noreply.github.com>
2025-07-01 14:25:37 +0530

    Update index.js

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -13,7 +13,7 @@ export const createNewOrder = createAsyncThunk(
   "/order/createNewOrder",
   async (orderData) => {
     const response = await axios.post(
-      "https://ecommerce-b5ff.onrender.com/api/shop/order/create",
+      "https://ecommerce-b5ff.onrender.com/shop/order/create",
       orderData
     );
 
@@ -25,7 +25,7 @@ export const capturePayment = createAsyncThunk(
   "/order/capturePayment",
   async ({ paymentId, payerId, orderId }) => {
     const response = await axios.post(
-      "https://ecommerce-b5ff.onrender.com/api/shop/order/capture",
+      "https://ecommerce-b5ff.onrender.com/shop/order/capture",
       {
         paymentId,
         payerId,
@@ -41,7 +41,7 @@ export const getAllOrdersByUserId = createAsyncThunk(
   "/order/getAllOrdersByUserId",
   async (userId) => {
     const response = await axios.get(
-      `https://ecommerce-b5ff.onrender.com/api/shop/order/list/${userId}`
+      `https://ecommerce-b5ff.onrender.com/shop/order/list/${userId}`
     );
 
     return response.data;
@@ -52,7 +52,7 @@ export const getOrderDetails = createAsyncThunk(
   "/order/getOrderDetails",
   async (id) => {
     const response = await axios.get(
-      `https://ecommerce-b5ff.onrender.com/api/shop/order/details/${id}`
+      `https://ecommerce-b5ff.onrender.com/shop/order/details/${id}`
     );
 
     return response.data;
